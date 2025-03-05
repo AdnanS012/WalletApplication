af1538123ad367361b0221eba15100f1f89f71be
adnan.shaikh_ftc <adnan.shaikh_ftc@external.swiggy.in>
2025-03-05 22:56:44 +0530

    Added Dockerfile and docker-compose.yml and containerized the whole application with database

--- src/main/java/com/example/WalletApp/WalletApplication.java
+++ src/main/java/com/example/WalletApp/WalletApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.WalletApp.Controller", "com.example.WalletApp.Service","com.example.WalletApp.Config","com.example.WalletApp.grpcClient"})
+@ComponentScan(basePackages = {"com.example.WalletApp.Controller", "com.example.WalletApp.Service","com.example.WalletApp.Config"})
 public class WalletApplication {
     public static void main(String[] args) {
         SpringApplication.run(WalletApplication.class, args);
